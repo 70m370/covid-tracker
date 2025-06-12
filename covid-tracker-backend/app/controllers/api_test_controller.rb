4815dd9a7608dee8a8488983e1292c0c5b8afd5c
70m370 <74630076+70m370@users.noreply.github.com>
2025-06-11 22:33:00 -0300

    service updated

--- covid-tracker-backend/app/controllers/api_test_controller.rb
+++ covid-tracker-backend/app/controllers/api_test_controller.rb
@@ -3,10 +3,9 @@ class ApiTestController < ApplicationController
 
   def covid
     # type can be cases or deaths
-    data = CovidApiService.fetch(params[:country], params[:type], params[:date])
+    data = CovidApiService.fetch(params[:country], params[:type], params[:start_date], params[:end_date])
     render json: data
   rescue => e
     render json: { error: e.message }, status: :bad_request # 400
   end
-  
 end
--- covid-tracker-backend/app/services/covid_api_service.rb
+++ covid-tracker-backend/app/services/covid_api_service.rb
@@ -2,7 +2,8 @@ class CovidApiService
   include HTTParty
   base_uri "https://api.api-ninjas.com/v1"
 
-  def self.fetch(country, type, date)
+  # fetch data from the api
+  def self.fetch(country, type, start_date, end_date)
     response = get("/covid19",
   query: {
     country: country,
@@ -12,6 +13,12 @@ class CovidApiService
     "X-Api-Key" => ENV["API_KEY"]
   })
 
+    date = start_date
+    # gambiarra
+    if end_date == nil
+      end_date = date
+    end
+
     if response.success?
 
       data = response.parsed_response
@@ -19,17 +26,19 @@ class CovidApiService
 
       country_data = data.first
 
+      # adapt this to a .map and extract the total and new cases on this range
+
       timeline_data = country_data[type.downcase] || {}
-
       specific_data = timeline_data[date]
-
+      
       unless specific_data
         raise StandardError, "No data found for type '#{type}' on date '#{date}'"
       end
       {
         total: specific_data["total"].to_s,
         new: specific_data["new"].to_s,
-        date: date
+        start_date: date,
+        end_date: end_date
       }
     else
       raise StandardError, "Failed to fetch Covid data"
