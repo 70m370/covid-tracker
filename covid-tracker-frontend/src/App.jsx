e6c60f96a0722ac14231f3c9b6373a3445ce548c
70m370 <74630076+70m370@users.noreply.github.com>
2025-06-15 12:16:16 -0300

    cors configured

--- covid-tracker-frontend/src/App.jsx
+++ covid-tracker-frontend/src/App.jsx
@@ -2,18 +2,20 @@ import { useState, useEffect } from 'react'
 import api from '../src/libs/axios'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0);
-  const [data, setDate] = useState(null);
+  const [data, setData] = useState(null);
 
+  // this will handle the results after
   useEffect(() => {
-    api.get('/covid_benchmarks')
+    api.get('/results')
       .then((res) => setData(res.data))
       .catch((err) => console.error(err));
   }, []);
 
 
-
+  console.log(data);
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold">COVID Tracker</h1>
